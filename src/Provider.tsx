080b98b41b90c7a791574bfcd0df3b1831637e75
youzi <youri@saulx.com>
2023-06-07 12:26:12 +0200

    fix: toggling routes should not break hook rules

--- src/Provider.tsx
+++ src/Provider.tsx
@@ -10,16 +10,33 @@ export const defaultRoute: RouterCtx = {
 
 export const RouterContext = createContext<RouterCtx>(defaultRoute)
 
+const DefaultRoutes: FC<{
+  children: ReactNode
+  path?: string
+  location?: Location
+}> = ({ children, path, location }) => {
+  return (
+    <RouterContext.Provider value={useRouterListeners(path, location)}>
+      {children}
+    </RouterContext.Provider>
+  )
+}
+
 // Replace with top level hook (the first)
 export const Router: FC<{
   children: ReactNode
   routes?: RouterRootCtx
   path?: string
   location?: Location
-}> = ({ children, routes, path = '', location }) => {
+}> = ({ children, routes, path, location }) => {
   if (!routes) {
-    routes = useRouterListeners(path, location)
+    return (
+      <DefaultRoutes path={path || ''} location={location}>
+        {children}
+      </DefaultRoutes>
+    )
   }
+
   return (
     <RouterContext.Provider value={routes}>{children}</RouterContext.Provider>
   )
